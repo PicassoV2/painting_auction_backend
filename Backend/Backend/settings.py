5c629a422cd8a9ba86185e1bf6b12a4a20c2517c
PicassoV2 <picassov2team@gmail.com>
2024-10-06 01:34:22 +0530

    Added CSRF_TRUSTED_ORIGINS

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -44,6 +44,8 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+CSRF_TRUSTED_ORIGINS = ['https://paintingauctionbackend-production.up.railway.app']
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
