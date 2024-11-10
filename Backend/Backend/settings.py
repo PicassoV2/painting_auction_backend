9ca115c01ba6f1aba0be4059aaba8c7f741d982d
bhaskarbhuyan <bhaskarbhuyan0012@gmail.com>
2024-11-10 23:35:52 +0530

    JWT expiration set to 30 days

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -156,8 +156,8 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
 CORS_ALLOWED_ORIGINS = [
